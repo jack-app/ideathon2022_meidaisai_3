888260a7f4598f04c0ad51bd4e7e65adad35a48c
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-06-10 22:39:05 +0900

    ローカルで動くものは完成

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask,request
+from flask import Flask,request, jsonify
 from api import API_translate
+from question_choice import Question
+
+
 import random
 
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def index():
@@ -11,14 +15,22 @@ def index():
 
 @app.route('/api')
 def respond():
-    count = request.args.get("count")
-    with open("./question_list.txt","r") as f:
-        question_list = f.read()
-        question_list = question_list.split("\n")
-    question = random.choice(question_list)
-    sample = API_translate(question)
-    sample = sample.translate_text(count)
-    return sample.json()
+    count = int(request.args.get("count"))
+    binary_str = request.args.get("b")
+    q = Question(binary_str, "./source.json")
+    translated_text = API_translate(q.question["content"]).translate_text(count)
+    print(q.wrongs)
+    res = {
+        "source": q.question["content"],
+        "translated": translated_text, 
+        "choices": {
+            "correct": q.question["title"],
+            "wrong": q.wrongs,
+        }, 
+        "finish": q.binary_str
+    }
+    
+    return jsonify(res)
 
 if __name__ == "__main__":
     app.run(debug=True)
