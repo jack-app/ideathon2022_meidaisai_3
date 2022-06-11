6de7dae4d6bb47251579b9b307cfb00ca75fdba7
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-06-12 01:12:28 +0900

    fix: constants/isLocal

--- backend/constants.py
+++ backend/constants.py
@@ -1,9 +1,10 @@
 import os
 class Constants:
-    def __init__(self, isLocal=os.environ["DONBURAKKO_IS_LOCAL"]):
-        if isLocal:
+    def __init__(self, isLocal=bool(os.environ["DONBURAKKO_IS_LOCAL"])):
+        print(isLocal=="True")
+        if isLocal=="True":
             self.prefix_source_dir = "."
             self.prefix_frontend_dir = "../frontend"
-        if not(isLocal):
+        else:
             self.prefix_source_dir = os.getcwd() + "/backend"
             self.prefix_frontend_dir = "../frontend"
