aceb0a980fae4f9f9945fa37e3dc1bd447c8cd66
kotama7 <kotamatakanori2@gmail.com>
2022-07-09 22:12:12 +0900

    #9 誰が一番早く反応したか確認できるようにしました。

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,7 @@ from constants import Constants
 all_user = []
 room_volume = [2] * 50 + [4] * 50
 room_condition = [True] * 100
+respondent = ''
 
 
 c = Constants()
@@ -47,7 +48,7 @@ def prepare():
         i += 1
     room = i
     all_user[room].append(name)
-    emit('battle_start', {'room' : room})
+    emit('room_set', {'room' : room})
     if room_volume[i] == len(all_user[i]):
         room_condition[i] = False
         emit('battle_start', {'ok_num': room}, broadcast = True)
@@ -63,6 +64,14 @@ def end():
     global room_condition
     room_condition[room] = True
     del all_user[room]
+
+@socketio.on('bottun_push')
+def push():
+    name = request.form.get('respondent')
+    global respondent
+    if respondent:
+        respondent = name
+    emit('respond',{'respondent': respondent})
 
 
 @app.route('/api')
